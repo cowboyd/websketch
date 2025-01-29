4773fb6bf534f0f033706eefbbc1679081ef1839
Charles Lowell <cowboyd@frontside.com>
2025-01-29 14:50:38 -0600

    Initial commit

---
+++ main.tsx
@@ -0,0 +1,39 @@
+import { main, suspend } from "effection";
+import { createRevolution, route } from "revolution";
+import { useTailwind } from "./plugins/tailwind.ts";
+import { assetRoute } from "./plugins/asset-route.ts";
+
+await main(function* () {
+  let css = yield* useTailwind({ input: "main.css", outdir: "build" });
+
+  let revolution = createRevolution({
+    app: [
+      route("/", function* () {
+        return (
+          <html lang="en-US" dir="ltr">
+            <head>
+              <meta charset="UTF-8" />
+              <title>Sketch some stuff</title>
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+              />
+              <link rel="stylesheet" href={css} />
+            </head>
+            <body>
+              <h1 class="text-3xl font-bold underline">
+                Hello world!
+              </h1>
+            </body>
+          </html>
+        );
+      }),
+      route("/build(.*)", assetRoute("build")),
+    ],
+  });
+
+  let server = yield* revolution.start();
+  console.log(`www -> http://localhost:${server.port}`);
+
+  yield* suspend();
+});
---
+++ plugins/tailwind.ts
@@ -0,0 +1,40 @@
+import { exists } from "jsr:@std/fs";
+import { join } from "jsr:@std/path";
+import { crypto } from "jsr:@std/crypto";
+import { encodeHex } from "jsr:@std/encoding/hex";
+import { x } from "jsr:@effection-contrib/tinyexec";
+import { call } from "effection";
+
+export interface TailwindOptions {
+  readonly input: string;
+  readonly outdir: string;
+}
+
+export function* useTailwind(options: TailwindOptions) {
+  let { input, outdir } = options;
+  let outpath = join(outdir, input);
+  let proc = yield* x("deno", [
+    "run",
+    "-A",
+    "npm:@tailwindcss/cli@^4.0.0",
+    "--input",
+    input,
+    "--output",
+    outpath,
+  ]);
+  let result = yield* proc;
+  if (result.stderr) {
+    console.log(result.stderr);
+  }
+  if (result.stdout) {
+    console.log(result.stdout);
+  }
+
+  if (yield* call(() => exists(outpath))) {
+    let content = yield* call(() => Deno.readFile(outpath));
+    const buffer = yield* call(() => crypto.subtle.digest("SHA-256", content));
+    const hash = encodeHex(buffer);
+    return `${outpath}?${hash}`;
+  }
+  throw new Error(`failed to generate ${outpath}`);
+}
